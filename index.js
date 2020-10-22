8ca390177b66f041fdd34d206c2f033b0bab8bca
Lui Fensmark Nielsencdcd..git config --global user.email 3862142@rts-365.dk <3862142@rts-365.dk>
2020-10-22 12:27:56 +0200

    add changes to README

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var reset = "\x1b[0m";
 
 var magicConsole = {
-    error: function(string) {
+    error: function (string) {
         console.error("\x1b[31m" + "ERROR:\t" + string + reset);
     },
-    warning: function(string) {
+    warning: function (string) {
         console.warn("\x1b[33m" + "WARNING:\t" + string + reset);
     },
-    success: function(string){
+    success: function (string) {
         console.info("\x1b[32m" + "SUCCESS:\t" + string + reset);
     },
-    magenta: function(string){
+    magenta: function (string) {
         console.log("\x1b[35m" + "MAGENTA:\t" + string + reset)
     }
 }
